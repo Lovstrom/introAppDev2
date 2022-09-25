aefc18f405cee3ee9d8af1528e0aa062a572412d
Sofia Lövström <sofia.lovstrom@outlook.com>
2022-09-25 17:45:50 +0200

    last fix

--- main.js
+++ main.js
@@ -1,69 +1,188 @@
 
-var i=0;    
-var xDie=0;
-var yDie=0;
-/**-------------This function does the gambling-------------------**/
+// -------------------------------------------------------------------------
+// Create Players
+// -------------------------------------------------------------------------
+
+var playerCount = 0;
+
+function enterPlayer(event){
+  event.preventDefault();
+  if(!((document.getElementById("enterPlayerName").value === "")
+    && (document.getElementById("enterCash").value === ""))) {
+    playerCount = ++playerCount;
+
+    // create a new div element
+    const newPlayerDiv = document.createElement('div');
+    newPlayerDiv.setAttribute('id','player'+playerCount);
+    //console.debug(newPlayerDiv.id);
+
+    const newPlayerNumber = document.createElement('h1');
+    newPlayerNumber.innerHTML = playerCount;
+   
+    const newPlayerName = document.createElement('h2');
+    newPlayerName.innerHTML = document.getElementById("enterPlayerName").value;
+    document.getElementById("enterPlayerName").value=null;
+ 
+    const newPlayerCash = document.createElement('h2');
+    newPlayerCash.setAttribute('id','player'+playerCount+'Cash');
+    newPlayerCash.innerHTML = document.getElementById("enterCash").value;
+    document.getElementById("enterCash").value=null;
+
+    newPlayerDiv.appendChild(newPlayerNumber); 
+    newPlayerDiv.appendChild(newPlayerName); 
+    newPlayerDiv.appendChild(newPlayerCash); 
+   
+    // Assign class to new Div
+    newPlayerDiv.classList.add('playerDiv');
+    // set color--- these are hard coded values, and I' aware thats not great, lazy solution.  
+    if(playerCount==1){
+      newPlayerDiv.style.backgroundColor = "#FF4040";
+    }
+    if(playerCount==2){
+      newPlayerDiv.style.backgroundColor = "#4040FF";
+    }
+    if(playerCount==3){
+      newPlayerDiv.style.backgroundColor = "#40FF40";
+    }
+    if(playerCount==4){
+      newPlayerDiv.style.backgroundColor = "#888";
+    }
+    // add the newly created element and its content into the DOM
+    const currentDiv = document.getElementById("createPlayersDiv");
+    currentDiv.appendChild(newPlayerDiv); 
+
+    let regStatus=document.getElementById("regStatus").innerHTML="";
+  }
+  else{
+    document.getElementById("regStatus").innerHTML="Please enter name and cash";
+  }
+}
+
+// -------------------------------------------------------------------------
+// Play a turn in The Dice Game
+// -------------------------------------------------------------------------
+
+var playerTurn = 1;
+
 function rollDicePlay() {
-  let betty = document.getElementById("bet").value;
-  let cash = document.getElementById("cashPot").value;
-  /**console.debug("betty" + betty);**/
-  /**----------Check for suffient funds and that its not a 0 bet  **/
- if(cash-betty>-1 && !(betty<1))  {
+  let regStatus=document.getElementById("regStatus");
+  let currentPlayer = document.getElementById("player"+playerTurn);
+  let currentPlayerCash = document.getElementById("player"+playerTurn+'Cash');
+  let playerBet = parseInt(document.getElementById("turnBet").value);
+
+  if (currentPlayer !== null){
+    regStatus.innerHTML="";
+    if (currentPlayerCash !== null){
+      if(!(playerBet === NaN)){
+        //console.debug("currentPlayerCash.innerHTML: "+currentPlayerCash.innerHTML);
+        //console.debug("playerBet: "+playerBet);
+      
+        if(currentPlayerCash.innerHTML>0 
+        && (currentPlayerCash.innerHTML>=playerBet) ){
+          // play dice  
+          currentPlayerCash.innerHTML = currentPlayerCash.innerHTML - playerBet;
+          //console.debug("currentPlayerCash.innerHTML - bet: "+currentPlayerCash.innerHTML);    
+          rollDice();     
+        }
+        else if(currentPlayerCash.innerHTML==0 ){
+            regStatus.innerHTML="You have no more money! Game Over";
+        }        
+        else if(playerBet>currentPlayerCash.innerHTML){
+          regStatus.innerHTML="You can't bet that high, try a lower number!";
+        }
+
+      }
+      else{
+        console.debug("!== NaN -> "+playerBet);
+        regStatus.innerHTML="Please place a valid bet, larger than 0!";
+      }  
+    }
+  }
+  else{
+      regStatus.innerHTML="Please create at least one player to play";
+  }
+}
+
+
+// -------------------------------------------------------------------------
+// Roll Dice and retur win factor 2, 1 or 0.
+// -------------------------------------------------------------------------
+
+
+
+function rollDice(){
+  let win=0;
+  let xDie=0;
+  let yDie=0;
+
   /** Random numers for the dice **/
   xDie=Math.floor(Math.random() * 6)+1;
   yDie=Math.floor(Math.random() * 6)+1;
-  let win=0;
-  /**  Calculate the winning or losing **/
+
   if (xDie===yDie) {
-    win=betty*2;
+    win=2;
   }    
   else if(xDie>yDie){
-    win=betty;  
+    win=1;  
   }
-  else if(yDie>xDie){
+  else if(xDie<yDie){
     win=0;
   }
-  document.getElementById("cashPot").value= parseInt(cash)-parseInt(betty)+parseInt(win);    
+
+  writeResult(xDie, yDie, win);
+
+  //return win;
+}
+
+// -------------------------------------------------------------------------
+// Play a turn in The Dice Game
+// -------------------------------------------------------------------------
+
+
+var turnCount=0;    
+
+function writeResult(xDie, yDie, win){
+
+  let currentPlayerCash = document.getElementById("player"+playerTurn+'Cash');
+  let playerBet = parseInt(document.getElementById("turnBet").value);
+
+  turnCount = ++turnCount;
   // Create an "li" node:
   const node = document.createElement("li");
   // Create a text node:
-  i=i+1; /** round count, not impacting anything */
-  const textnode = document.createTextNode(i+": you rolled: "+xDie+" and "+ yDie+ "you win: " + win );
-  if (xDie===yDie) {
-    node.style.backgroundColor = "#40FF40";
+  const textnode = document.createTextNode("turn: "+turnCount+"; you rolled: "+xDie+" and "+ yDie+ " you win: bet *" + win );
+    if(win===0){
+      node.style.backgroundColor = "#FF8888";   
+      
+    }   
+    if(win===1){
+      node.style.backgroundColor = "#FF8800";
+      currentPlayerCash.innerHTML = parseInt(currentPlayerCash.innerHTML) + playerBet;
     }    
-    else if(xDie>yDie){
-      node.style.backgroundColor = "#FFAA44";
-    }
-    else{
-      node.style.backgroundColor = "#FF8888";    
-    }
+    if(win===2){
+      node.style.backgroundColor = "#00FF00";
+      currentPlayerCash.innerHTML = parseInt(currentPlayerCash.innerHTML) + (playerBet*2);
+    }   
+
+ 
+  
   
     // Append the text node to the "li" node:
-      node.appendChild(textnode);
-    // Append the "li" node to the list:
-    document.getElementById("myList").appendChild(node);   
-  }
-  else {
-    if (betty<1) {
-      alert("you must bet at least 1")
-    }
-    else {alert("you don't have any money!!! Game over, Go home!")}
-  }
+  node.appendChild(textnode);
+  // Append the "li" node to the list:
+  document.getElementById("turnResults").appendChild(node);   
 }
 
 
-function setStart() {
-  document.getElementById("gameName").innerHTML = "You clicked the button, I am new paragraph.";
- 
+// -------------------------------------------------------------------------
+// Bonus function, move element with timeout until 500 pixels left.
+// -------------------------------------------------------------------------
+function moveDiv(divID) {
+  if(parseInt(document.getElementById(divID).style.left) < 500 ){
+   document.getElementById(divID).style.left = (parseInt(document.getElementById(divID).style.left)+1)+"px";
+	if (parseInt(document.getElementById(divID).style.left) > 1) setTimeout("moveDiv('"+divID+"')", 10);
+ }
 }
+	
 
-function moveButton()
-{
-  for (let q = 0; q < 900; q++)
-   {  
-      document.getElementById("movable").style.left = `${q}px`;
-   }
-}   
-  
 
